154ec165d5fd9c65e2f8afabab530cb0ba6a1946
hanifr <irhaniframli@gmail.com>
2021-08-16 13:35:37 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -35,6 +35,12 @@ echo "${_CYAN}You have entered $_domain for your MQTT domain name${_RESET}"
 echo "[*] Creating GPS data acquisition script"
 
 cat >/tmp/gps_simple.py <<EOL
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+# Simple GPS module demonstration.
+# Will wait for a fix and print a message every second with the current location
+# and other details.
 import time
 import board
 import busio
@@ -43,13 +49,39 @@ import adafruit_gps
 
 import context  # Ensures paho is in PYTHONPATH
 import paho.mqtt.publish as publish
+import json
+
+# Create a serial connection for the GPS connection using default speed and
+# a slightly higher timeout (GPS modules typically update once a second).
+# These are the defaults you should use for the GPS FeatherWing.
+# For other boards set RX = GPS module TX, and TX = GPS module RX pins.
+#uart = busio.UART(board.TX, board.RX, baudrate=9600, timeout=10)
 
 # for a computer, use the pyserial library for uart access
 import serial
 uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=10)
 
+# If using I2C, we'll create an I2C interface to talk to using default pins
+# i2c = board.I2C()
+
 # Create a GPS module instance.
 gps = adafruit_gps.GPS(uart, debug=False)  # Use UART/pyserial
+# gps = adafruit_gps.GPS_GtopI2C(i2c, debug=False)  # Use I2C interface
+
+# Initialize the GPS module by changing what data it sends and at what rate.
+# These are NMEA extensions for PMTK_314_SET_NMEA_OUTPUT and
+# PMTK_220_SET_NMEA_UPDATERATE but you can send anything from here to adjust
+# the GPS module behavior:
+#   https://cdn-shop.adafruit.com/datasheets/PMTK_A11.pdf
+
+# Turn on the basic GGA and RMC info (what you typically want)
+gps.send_command(b"PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0")
+# Turn on just minimum info (RMC only, location):
+# gps.send_command(b'PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
+# Turn off everything:
+# gps.send_command(b'PMTK314,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
+# Turn on everything (not all of it is parsed!)
+# gps.send_command(b'PMTK314,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0')
 
 # Set update rate to once a second (1hz) which is what you typically want.
 gps.send_command(b"PMTK220,1000")
@@ -78,10 +110,20 @@ while True:
             continue
         # We have a fix! (gps.has_fix is true)
         # Print out details about the fix like location, date, etc.
-
+#        print("=" * 40)  # Print a separator line.
+#        print(
+#            "Fix timestamp: {}/{}/{} {:02}:{:02}:{:02}".format(
+#                gps.timestamp_utc.tm_mon,  # Grab parts of the time from the
+#                gps.timestamp_utc.tm_mday,  # struct_time object that holds
+#                gps.timestamp_utc.tm_year,  # the fix time.  Note you might
+#                gps.timestamp_utc.tm_hour,  # not get all data like year, day,
+#                gps.timestamp_utc.tm_min,  # month!
+#                gps.timestamp_utc.tm_sec,
+#            )
+#        )
         LAT = "{0:.6f}".format(gps.latitude)
         LON = "{0:.6f}".format(gps.longitude)
-        print("Fix quality: {}".format(gps.fix_quality))
+        FIX = "Fix quality: {}".format(gps.fix_quality)
         
         # Some attributes beyond latitude, longitude and timestamp are optional
         # and might not be present.  Check if they're None before trying to use!
@@ -97,9 +139,8 @@ while True:
 #        HDIL = "Horizontal dilution: {}".format(gps.horizontal_dilution)
 #        if gps.height_geoid is not None:
 #        HGEO = "Height geo ID: {} meters".format(gps.height_geoid)
-        #STR = '{'+ LAT + ','+ LON + '}'
-        print ({LAT, LON})
-        publish.single("$_domain", "{\"LAT\":" + LAT+ ",\"LON\":"+ LON +"}", hostname="nex.airmode.live")
+#        print ({LAT, LON})
+        publish.single("nexplex/sense", "{\"GID\":" + "301" + ",\"LAT\":"+LAT+",\"LON\":"+ LON +"}", hostname="$_domain")
 EOL
 sudo mv /tmp/gps_simple.py /home/pi/paho.mqtt.python/examples/gps_simple.py
 
