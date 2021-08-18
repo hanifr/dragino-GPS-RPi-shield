2d50fff45f81bc298fac8b4931aee6599c20e5a7
hanifr <irhaniframli@gmail.com>
2021-08-18 16:03:25 +0800

    Update startupgps.sh

--- startupgps.sh
+++ startupgps.sh
@@ -9,37 +9,49 @@ _CYAN=`tput setaf 6`
 _RESET=`tput sgr0`
 # printing greetings
 
-echo "${_MAGENTA}Setup Progress....Creating GPS startup service:: started${_RESET}"
+echo "${_MAGENTA}Setup Progress....Creating GPS and LoRa startup service:: started${_RESET}"
 echo
-sudo cat >/tmp/startup.sh <<EOL
+sudo cat >/tmp/startlora.sh <<EOL
 #!/bin/bash
 
 # cd /home/pi/dragino-GPS-RPi-shield/paho.mqtt.python/examples/
 sudo python /home/pi/dragino-GPS-RPi-shield/paho.mqtt.python/examples/LoRaRX.py
-sudo python3 /home/pi/dragino-GPS-RPi-shield/paho.mqtt.python/examples/gps_simple.py >> /home/pi/dragino-GPS-RPi-shield/paho.mqtt.python/examples.log 2>&1
 
 EOL
-sudo mv /tmp/startup.sh /home/pi/dragino-GPS-RPi-shield/paho.mqtt.python/examples/startup.sh
-sudo chmod 744 /home/pi/dragino-GPS-RPi-shield/paho.mqtt.python/examples/startup.sh
-sudo cat >/tmp/startgps.service <<EOL
+sudo mv /tmp/startlora.sh /home/pi/dragino-GPS-RPi-shield/paho.mqtt.python/examples/startlora.sh
+sudo chmod 744 /home/pi/dragino-GPS-RPi-shield/paho.mqtt.python/examples/startlora.sh
+
+sudo cat >/tmp/startgps.sh <<EOL
+#!/bin/bash
+
+# cd /home/pi/dragino-GPS-RPi-shield/paho.mqtt.python/examples/
+sudo python3 /home/pi/dragino-GPS-RPi-shield/paho.mqtt.python/examples/gps_simple.py
+
+EOL
+sudo mv /tmp/startgps.sh /home/pi/dragino-GPS-RPi-shield/paho.mqtt.python/examples/startgps.sh
+sudo chmod 744 /home/pi/dragino-GPS-RPi-shield/paho.mqtt.python/examples/startgps.sh
+
+sudo cat >/tmp/startdragino.service <<EOL
 [Unit]
 Description=GPS module service
 [Service]
 Type=simple
 # WorkingDirectory=/home/pi/dragino-GPS-RPi-shield/paho.mqtt.python/examples
-ExecStart=/home/pi/dragino-GPS-RPi-shield/paho.mqtt.python/examples/startup.sh
+ExecStartPre=/home/pi/dragino-GPS-RPi-shield/paho.mqtt.python/examples/startlora.sh
+ExecStart=/home/pi/dragino-GPS-RPi-shield/paho.mqtt.python/examples/startgps.sh
+Restart=on-failure
 # User=do-user
 [Install]
 WantedBy=multi-user.target
 EOL
-sudo mv /tmp/startgps.service /etc/systemd/system/startgps.service
-echo "${_YELLOW}[*] Starting startgps systemd service${_RESET}"
-sudo chmod 664 /etc/systemd/system/startgps.service
+sudo mv /tmp/startdragino.service /etc/systemd/system/startdragino.service
+echo "${_YELLOW}[*] Starting startdragino systemd service${_RESET}"
+sudo chmod 664 /etc/systemd/system/startdragino.service
 sudo systemctl daemon-reload
-sudo systemctl enable startgps.service
-sudo systemctl start startgps.service
-echo "${_YELLOW}To see GPS startup service logs run \"sudo journalctl -u startgps -f\" command${_RESET}"
+sudo systemctl enable startdragino.service
+sudo systemctl start startdragino.service
+echo "${_YELLOW}To see GPS startup service logs run \"sudo journalctl -u startdragino -f\" command${_RESET}"
 
-echo "${_MAGENTA}Setup Progress....Creating GPS startup service:: finished${_RESET}"
+echo "${_MAGENTA}Setup Progress....Creating GPS and LoRa startup service:: finished${_RESET}"
 echo
  
